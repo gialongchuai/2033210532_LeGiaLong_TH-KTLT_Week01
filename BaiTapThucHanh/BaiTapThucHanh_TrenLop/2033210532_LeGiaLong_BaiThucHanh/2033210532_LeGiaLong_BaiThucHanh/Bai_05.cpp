e07dedeff125db63451978c0aca554c89eea8a6f
gialongchuai <minhtan0949@gmail.com>
2024-07-12 18:33:31 +0700

    first commit

---
+++ BaiTapThucHanh/BaiTapThucHanh_TrenLop/2033210532_LeGiaLong_BaiThucHanh/2033210532_LeGiaLong_BaiThucHanh/Bai_05.cpp
@@ -0,0 +1,28 @@
+﻿/*
+	Bài 05: Nhập 3 số nguyên a, b, c. Xuất kết quả c/(a-b).
+*/
+
+#include <stdio.h>
+#include <stdlib.h> // Thu vien de su dung "pause" dung man hinh de xem ket qua
+
+int main() {
+	int a, b, c;
+	printf("Nhap so nguyen a: ");
+	scanf("%d", &a);
+	printf("Nhap so nguyen b: ");
+	scanf("%d", &b);
+	printf("Nhap so nguyen c: ");
+	scanf("%d", &c);
+
+	// Kiem tra dieu kien a va b khong bang nhau de tranh chia cho 0
+	if (a == b) {
+		printf("Loi: Phep chia cho 0 khong hop le.\n");
+	}
+	else {
+		float result = (float)c / (a - b);
+		printf("Ket qua cua phep chia c/(a-b) la: %.2f\n", result);
+	}
+
+	system("pause"); // Dung man hinh xem ket qua
+	return 0;
+}
